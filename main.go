db0a30ef2ead6a22089fd336780200992e1d2795
Shou Tseng <yakushou730@gmail.com>
2021-02-21 10:44:50 +0800

    Add user entity

---
+++ main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"github.com/yakushou730/go-ent-test/ent/user"
+
+	"github.com/yakushou730/go-ent-test/ent"
+
+	_ "github.com/mattn/go-sqlite3"
+)
+
+func main() {
+	client, err := ent.Open("sqlite3", "file:ent?mode=memory&cache=shared&_fk=1")
+	if err != nil {
+		log.Fatalf("failed opening connection to sqlite: %v", err)
+	}
+	defer client.Close()
+
+	// Run the auto migration tool.
+	if err := client.Schema.Create(context.Background()); err != nil {
+		log.Fatalf("failed creaating schema resources: %v", err)
+	}
+
+	CreateUser(context.Background(), client)
+	QueryUser(context.Background(), client)
+}
+
+func CreateUser(ctx context.Context, client *ent.Client) (*ent.User, error) {
+	u, err := client.User.Create().SetAge(30).SetName("a8m").Save(ctx)
+	if err != nil {
+		return nil, fmt.Errorf("failed creating user: %v", err)
+	}
+	log.Println("user was created: ", u)
+	return u, nil
+}
+
+func QueryUser(ctx context.Context, client *ent.Client) (*ent.User, error) {
+	u, err := client.User.Query().Where(user.NameEQ("a8m")).Only(ctx)
+	// `Only` fails if no user found, or more than 1 user returned.
+	if err != nil {
+		return nil, fmt.Errorf("failed querying user: %v", err)
+	}
+	log.Println("user returned: ", u)
+	return u, nil
+}
